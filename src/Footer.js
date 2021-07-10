41243bb54ad1af5883e1d8a0dd92a96667d86497
YirimS <yirimseck@gmail.com>
2021-07-10 15:43:50 -0700

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <>
+      <p>Created by Yirim Seck</p>
+      </>
+    );
+  }
+}
+
+export default Footer;
