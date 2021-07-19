f34b178371fb688e171ea03c0c57d4dd3d073d8f
YirimS <yirimseck@gmail.com>
2021-07-19 10:35:02 -0700

    worked on wallet fuction on main

--- src/Main.js
+++ src/Main.js
@@ -5,15 +5,34 @@ import CardColumns from 'react-bootstrap/CardColumns';
 import { Row } from 'react-bootstrap';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 100,
+    }
+  }
+
+  walletBalance = () => {
+    this.setState({
+      balance: this.state.balance - 5,
+    });
+  }
+
   render() {
     return (
       <main>
         <Button variant="success">Click</Button>
         <CardColumns>
+
+          <h3>balance: ${this.state.balance}</h3>
+
           <Row xs={1} md={2}>
-          {this.props.data.map((beast, index) => (
-            <HornedBeast key={index} title={beast.title} description={beast.description} imageUrl={beast.image_url} />
-          ))}
+            {this.props.data.map((beast, index) => (
+              <HornedBeast
+                balanceProperty={this.walletBalance}
+                key={index} title={beast.title} description={beast.description} imageUrl={beast.image_url} />
+            ))}
+
           </Row>
         </CardColumns>
 
